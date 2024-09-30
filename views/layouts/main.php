9549ab9261dc3d2ea29ae83ccadcd357484a854c
Debiason <debiasonw@gmail.com>
2024-09-30 16:01:54 -0300

    a

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,12 +17,12 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/deyraka/yii2
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
     <head>
-        <!-- <meta charset="<?= Yii::$app->charset ?>">
-        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" name="viewport"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/> -->
+        <meta charset="<?= Yii::$app->charset ?>">
+        <!-- <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" name="viewport"/> -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/> 
 
-        <!-- <link rel="stylesheet" type="text/css"
-              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700&display=swap"/> -->
+        <link rel="stylesheet" type="text/css"
+              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700&display=swap"/>
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
         <!-- FOR CHART -->
